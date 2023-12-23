636ca2f5ba983b59ab538853aedd0190003b0d2f
63234abstract <yanzhe.li@rakuten.com>
2023-12-23 20:38:36 +0900

    detected first numerical value by checking convertion greateer than 0

--- src/csv_preprocessor/process_csv.py
+++ src/csv_preprocessor/process_csv.py
@@ -3,23 +3,23 @@ import numpy as np
 import os
 import sys
 
-sys.path.append("/Users/yanzhe.li/Documents/finance/sec_clean/src")
+sys.path.append("/Users/yanzhe.li/Documents/sec_webscraping/src")
 import config
 import export_dataframe
 
 
 def is_number(s):
     try:
-        float(s)
-        return float(s)
+        if float(s) > 0:
+            return float(s)
     except ValueError:
         return False
 
 
 def get_first_number(series):
-    for item in series:
-        if is_number(item):
-            return is_number(item)
+    for value in series.values:
+        if is_number(value):
+            return is_number(value)
     return np.nan
 
 
@@ -29,19 +29,8 @@ def csv_runner(csv_dir, keywords):
         df = pd.read_csv(os.path.join(csv_dir, file))
         result_df = extract_intangile_form(df, keywords)
         result_df.to_csv(
-            f"/Users/yanzhe.li/Documents/finance/sec_clean/data/extracted_csv/{file}"
+            f"/Users/yanzhe.li/Documents/sec_webscraping/data/extracted_csv/{file}"
         )
-
-
-# def filter_by_keywords(original_df, keywords):
-#     # mask = original_df.iloc[:, 0].str.lower().isin(keywords)  # ??
-#     mask = (
-#         original_df.iloc[:, 0]
-#         .astype(str)
-#         .str.lower()
-#         .apply(lambda x: any(keyword in x for keyword in keywords))
-#     )
-#     return original_df[mask]
 
 
 def extract_intangile_form(original_df, keywords):
@@ -52,7 +41,7 @@ def extract_intangile_form(original_df, keywords):
 
 
 if __name__ == "__main__":
-    data_dir = "/Users/yanzhe.li/Documents/finance/sec_clean/data"
+    data_dir = "/Users/yanzhe.li/Documents/sec_webscraping/data"
     target_dir = "test"
     csv_dir = os.path.join(data_dir, target_dir)
     keywords = config.Intangible_keywords
